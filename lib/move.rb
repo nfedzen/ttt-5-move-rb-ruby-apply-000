be475d202331ab90bd092a266b08d9bf6c83180a
Nathan Fedzen <nfedzen@hotmail.com>
2020-10-15 22:44:40 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,8 +7,8 @@ def display_board(board)
 end
 
 def input_to_index(choice)
-  choice.to_i
-  choice = choice - 1
+  number = choice.to_i - 1
+  
   
 end
 # code your input_to_index and move method here!
